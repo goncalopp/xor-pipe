d59bee0da00a3ef7a3387ba99e4c9eb4ba34e3e0
goncalopp <goncalopp@goncalopp.com>
2008-12-22 19:19:38 +0000

    added -f switch to write to original file instead of stdout; made sure -t implies not(-r)

--- xor.c
+++ xor.c
@@ -7,13 +7,13 @@ int min( int a, int b) {if (a<b) return a; return b;}
 
 int main(int argc, char *argv[])
 {
-unsigned char usagemessage[]= "Usage: xor [OPTION] FILE\nXORs stdin with file contents outputting to stdout.\nRepeats FILE if it is shorter than stdin length.\n\n  -t     do not repeat FILE (truncates to FILE length)\n  -r     repeats stdin instead of FILE\n";
+unsigned char usagemessage[]= "Usage: xor [OPTION] FILE\nXORs stdin with file contents outputting to stdout.\nRepeats FILE if it is shorter than stdin length.\n\n  -t     do not repeat FILE (truncates to FILE length)\n  -r     repeats stdin instead of FILE\n  -f     writes to original file instead of stdin; assumes -t\n";
 if (argc==1)
 	{puts(usagemessage);
 	exit(0);
 	}
 
-int repeatfromstdin = 0, truncate = 0;
+int repeatfromstdin = 0, truncate = 0, writetofile=0;
 
 FILE *infile;	
 unsigned char filebuffer[512];
@@ -22,7 +22,7 @@ int nreadfile, nreadstdin;
 int i;
 
 
-while ((i = getopt (argc, argv, "rt")) != -1)
+while ((i = getopt (argc, argv, "rtf")) != -1)
 	switch (i)
 		{
 		case 'r':
@@ -31,13 +31,20 @@ while ((i = getopt (argc, argv, "rt")) != -1)
 		case 't':
 			truncate = 1;
 			break;
+		case 'f':
+			writetofile = 1;
+			truncate = 1;
+			break;
 		case '?':
 			return -1;
 		} 
 
 
+if (writetofile==1)
+	infile=fopen(argv[optind], "r+");
+else
+	infile=fopen(argv[optind], "r");
 
-infile=fopen(argv[optind], "r");
 if (fseek(infile, 0, SEEK_SET)) 
 	{
 	fputs("Error opening file\n", stderr);
@@ -45,7 +52,7 @@ if (fseek(infile, 0, SEEK_SET))
 	}
 
 
-if (repeatfromstdin==0 )
+if (repeatfromstdin==0 || truncate==1)
 	{
 	do
 		{
@@ -53,6 +60,12 @@ if (repeatfromstdin==0 )
 		nreadfile=fread(&filebuffer, 1, 512, infile);
 		for (i=0; i< min(nreadstdin, nreadfile); i++)
 			stdinbuffer[i]^=filebuffer[i];
+		if (writetofile==1)
+			{
+			fseek(infile, -min(nreadstdin, nreadfile), SEEK_CUR);
+			fwrite(&stdinbuffer, 1, min(nreadstdin, nreadfile), infile);
+			}
+		else		
 		fwrite(&stdinbuffer, 1, min(nreadstdin, nreadfile), stdout);
 		if (nreadfile < 512) 
 			if (truncate==1)
